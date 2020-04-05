cce1125d9b9b6086b25d5cf1a68824f899d23b7b
oimarufoi <56217322+oimarufoi@users.noreply.github.com>
2020-04-06 00:42:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,29 @@
-from discord.ext import commands
+import discord
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.event
+async def on_ready():
+    print("logged in as " + client.user.name)
 
-@bot.event
+@client.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        msg = message.author.mention + "おはよう！いい朝だね！！"
+        await client.send_message(message.channel, msg)
+    
+@client.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 
-bot.run(token)
+client.run("token")
